4038ef33367eff9bd5e7eb6577a03036a9b58bf5
RodgersBrian <108925981+rodgersBRY@users.noreply.github.com>
2026-09-01 18:12:52 +0300

    REFACTOR: delete src/_legacy now that every route has been ported

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", ".next", "src/_legacy/**"] },
+  { ignores: ["dist", ".next"] },
   ...nextCoreWebVitals,
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
